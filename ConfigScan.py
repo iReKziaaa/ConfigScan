22dbbb0d6c0f906603691d69d4e70db1749f455b
iReKziaaa <contact@atelier-du-code.fr>
2026-07-11 12:04:26 +0200

    Add files via upload

---
+++ ConfigScan.py
@@ -0,0 +1,12 @@
+"""
+ConfigScan
+Ce dépôt distribue l'exécutable via les Releases.
+Le code source complet n'est pas publié dans ce dépôt.
+"""
+
+
+class ConfigScan:
+    """Utilitaire Windows pour scanner et afficher la configuration système."""
+
+    # Exécutable disponible dans les Releases GitHub.
+    pass
